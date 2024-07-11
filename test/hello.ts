4d58d694800ac87481699170d74f7dbe56b8399d
iamvigneshwars <iamvigneshwars@gmail.com>
2024-07-11 16:48:20 +0100

    fix module not found

---
+++ test/hello.ts
@@ -0,0 +1,14 @@
+// let a: Number = 1;
+// let b: string = a + "1";
+// console.log(a + b)
+
+
+// function test(a: String, b: string) {
+//     return a + b;
+// }
+
+// console.log(test("1", "1"))
+
+const test: String = "hello";
+
+console.log(test)
---
+++ test/test.js
@@ -0,0 +1,9 @@
+// function add(a, b) {
+//     return a + b;
+// }
+
+// console.log(add(1, 2))
+
+const hello = "message";
+
+hello();

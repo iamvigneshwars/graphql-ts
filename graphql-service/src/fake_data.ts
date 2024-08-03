e3bb708cee5ca392a598d51d4fb471c2169fba55
iamvigneshwars <iamvigneshwars@gmail.com>
2024-08-03 20:45:03 +0100

    update id type to string

--- graphql-service/src/fake_data.ts
+++ graphql-service/src/fake_data.ts
@@ -1,278 +1,275 @@
-export const namespaces: string[] = [
-    "namespace-1", "namespace-2"
-]
+export const namespaces: string[] = ["namespace-1", "namespace-2"];
 
 export const workflows = [
-    {
-        id: 1,
-        namespace: "namespace-1",
-        name: "Workflow 1",
-        status: "completed",
-    },
-    {
-        id: 2,
-        namespace: "namespace-1",
-        name: "Workflow 2",
-        status: "running",
-    },
-    {
-        id: 3,
-        namespace: "namespace-1",
-        name: "Workflow 3",
-        status: "completed",
-    },
-    {
-        id: 4,
-        namespace: 1,
-        name: "Workflow 4",
-        status: "failed",
-    },
-    {
-        id: 5,
-        namespace: "namespace-1",
-        name: "Workflow 5",
-        status: "pending",
-    },
-    {
-        id: 6,
-        namespace: "namespace-1",
-        name: "Workflow 6",
-        status: "pending",
-    },
-    {
-        id: 7,
-        namespace: "namespace-1",
-        name: "Workflow 7",
-        status: "completed",
-    },
-    {
-        id: 8,
-        namespace: "namespace-1",
-        name: "Workflow 8",
-        status: "failed",
-    },
-    {
-        id: 9,
-        namespace: "namespace-1",
-        name: "Workflow 9",
-        status: "failed",
-    },
-    {
-        id: 10,
-        namespace: "namespace-1",
-        name: "Workflow 10",
-        status: "pending",
-    },
-    {
-        id: 11,
-        namespace: "namespace-1",
-        name: "Workflow 11",
-        status: "completed",
-    },
-    {
-        id: 12,
-        namespace: "namespace-1",
-        name: "Workflow 12",
-        status: "completed",
-    },
-    {
-        id: 13,
-        namespace: "namespace-1",
-        name: "Workflow 13",
-        status: "completed",
-    },
-    {
-        id: 14,
-        namespace: "namespace-1",
-        name: "Workflow 14",
-        status: "pending",
-    },
-    {
-        id: 15,
-        namespace: "namespace-1",
-        name: "Workflow 15",
-        status: "completed",
-    },
-    {
-        id: 16,
-        namespace: "namespace-1",
-        name: "Workflow 16",
-        status: "running",
-    },
-    {
-        id: 17,
-        namespace: "namespace-1",
-        name: "Workflow 17",
-        status: "pending",
-    },
-    {
-        id: 18,
-        namespace: "namespace-1",
-        name: "Workflow 18",
-        status: "pending",
-    },
-    {
-        id: 19,
-        namespace: "namespace-1",
-        name: "Workflow 19",
-        status: "completed",
-    },
-    {
-        id: 20,
-        namespace: "namespace-1",
-        name: "Workflow 20",
-        status: "completed",
-    },
-    {
-        id: 21,
-        namespace: "namespace-2",
-        name: "Workflow 21",
-        status: "failed",
-    },
-    {
-        id: 22,
-        namespace: "namespace-2",
-        name: "Workflow 22",
-        status: "completed",
-    },
-    {
-        id: 23,
-        namespace: "namespace-2",
-        name: "Workflow 23",
-        status: "failed",
-    },
-    {
-        id: 24,
-        namespace: "namespace-2",
-        name: "Workflow 24",
-        status: "completed",
-    },
-    {
-        id: 25,
-        namespace: "namespace-2",
-        name: "Workflow 25",
-        status: "failed",
-    },
-    {
-        id: 26,
-        namespace: "namespace-2",
-        name: "Workflow 26",
-        status: "completed",
-    },
-    {
-        id: 27,
-        namespace: "namespace-2",
-        name: "Workflow 27",
-        status: "failed",
-    },
-    {
-        id: 28,
-        namespace: "namespace-2",
-        name: "Workflow 28",
-        status: "completed",
-    },
-]
+  {
+    id: "1",
+    namespace: "namespace-1",
+    name: "Workflow 1",
+    status: "completed",
+  },
+  {
+    id: "2",
+    namespace: "namespace-1",
+    name: "Workflow 2",
+    status: "running",
+  },
+  {
+    id: "3",
+    namespace: "namespace-1",
+    name: "Workflow 3",
+    status: "completed",
+  },
+  {
+    id: "4",
+    namespace: "namespace-1",
+    name: "Workflow 4",
+    status: "failed",
+  },
+  {
+    id: "5",
+    namespace: "namespace-1",
+    name: "Workflow 5",
+    status: "pending",
+  },
+  {
+    id: "6",
+    namespace: "namespace-1",
+    name: "Workflow 6",
+    status: "pending",
+  },
+  {
+    id: "7",
+    namespace: "namespace-1",
+    name: "Workflow 7",
+    status: "completed",
+  },
+  {
+    id: "8",
+    namespace: "namespace-1",
+    name: "Workflow 8",
+    status: "failed",
+  },
+  {
+    id: "9",
+    namespace: "namespace-1",
+    name: "Workflow 9",
+    status: "failed",
+  },
+  {
+    id: "10",
+    namespace: "namespace-1",
+    name: "Workflow 10",
+    status: "pending",
+  },
+  {
+    id: "11",
+    namespace: "namespace-1",
+    name: "Workflow 11",
+    status: "completed",
+  },
+  {
+    id: "12",
+    namespace: "namespace-1",
+    name: "Workflow 12",
+    status: "completed",
+  },
+  {
+    id: "13",
+    namespace: "namespace-1",
+    name: "Workflow 13",
+    status: "completed",
+  },
+  {
+    id: "14",
+    namespace: "namespace-1",
+    name: "Workflow 14",
+    status: "pending",
+  },
+  {
+    id: "15",
+    namespace: "namespace-1",
+    name: "Workflow 15",
+    status: "completed",
+  },
+  {
+    id: "16",
+    namespace: "namespace-1",
+    name: "Workflow 16",
+    status: "running",
+  },
+  {
+    id: "17",
+    namespace: "namespace-1",
+    name: "Workflow 17",
+    status: "pending",
+  },
+  {
+    id: "18",
+    namespace: "namespace-1",
+    name: "Workflow 18",
+    status: "pending",
+  },
+  {
+    id: "19",
+    namespace: "namespace-1",
+    name: "Workflow 19",
+    status: "completed",
+  },
+  {
+    id: "20",
+    namespace: "namespace-1",
+    name: "Workflow 20",
+    status: "completed",
+  },
+  {
+    id: "21",
+    namespace: "namespace-2",
+    name: "Workflow 21",
+    status: "failed",
+  },
+  {
+    id: "22",
+    namespace: "namespace-2",
+    name: "Workflow 22",
+    status: "completed",
+  },
+  {
+    id: "23",
+    namespace: "namespace-2",
+    name: "Workflow 23",
+    status: "failed",
+  },
+  {
+    id: "24",
+    namespace: "namespace-2",
+    name: "Workflow 24",
+    status: "completed",
+  },
+  {
+    id: "25",
+    namespace: "namespace-2",
+    name: "Workflow 25",
+    status: "failed",
+  },
+  {
+    id: "26",
+    namespace: "namespace-2",
+    name: "Workflow 26",
+    status: "completed",
+  },
+  {
+    id: "27",
+    namespace: "namespace-2",
+    name: "Workflow 27",
+    status: "failed",
+  },
+  {
+    id: "28",
+    namespace: "namespace-2",
+    name: "Workflow 28",
+    status: "completed",
+  },
+];
 
 export const tasks = [
-    {
-        id: 1,
-        workflow_id: 1,
-        name: "task-1",
-        status: "completed",
-    },
-    {
-        id: 2,
-        parent_task: 1,
-        workflow_id: 1,
-        name: "task-2 DKJFOIEJFOIJEOFIJIKJLSKDMFODSJFLKSDJFLKSD",
-        status: "completed"
-    },
-    {
-        id: 3,
-        parent_task: 1,
-        workflow_id: 1,
-        name: "task-3",
-        status: "pending"
-    },
-    {
-        id: 4,
-        workflow_id: 1,
-        name: "task-4",
-        status: "running"
-    },
-    {
-        id: 5,
-        workflow_id: 2,
-        name: "task-5",
-        status: "pending"
-    },
-    {
-        id: 20,
-        parent_task: 3,
-        workflow_id: 1,
-        name: "task-20",
-        status: "running"
-    },
-    {
-        id: 30,
-        parent_task: 20,
-        workflow_id: 1,
-        name: "task-30",
-        status: "running"
-    },
-    {
-        id: 21,
-        parent_task: 3,
-        workflow_id: 1,
-        name: "task-21",
-        status: "pending"
-    },
-    {
-        id: 6,
-        workflow_id: 2,
-        name: "task-6",
-        status: "pending"
-    },
-    {
-        id: 7,
-        workflow_id: 2,
-        name: "task-7",
-        status: "completed"
-    },
-    {
-        id: 8,
-        workflow_id: 3,
-        name: "task-8",
-        status: "completed"
-    },
-    {
-        id: 9,
-        workflow_id: 3,
-        name: "task-9",
-        status: "completed"
-    },
-    {
-        id: 10,
-        workflow_id: 4,
-        name: "task-10",
-        status: "running"
-    },
-    {
-        id: 11,
-        workflow_id: 4,
-        name: "task-11",
-        status: "running"
-    },
-    {
-        id: 12,
-        workflow_id: 4,
-        name: "task-12",
-        status: "running"
-    },
-    {
-        id: 13,
-        workflow_id: 5,
-        name: "task-13",
-        status: "completed"
-    },
-]
+  {
+    id: "1",
+    workflow_id: "1",
+    name: "task-1",
+    status: "completed",
+  },
+  {
+    id: "2",
+    parent_task: "1",
+    workflow_id: "1",
+    name: "task-2 DKJFOIEJFOIJEOFIJIKJLSKDMFODSJFLKSDJFLKSD",
+    status: "completed",
+  },
+  {
+    id: "3",
+    parent_task: "1",
+    workflow_id: "1",
+    name: "task-3",
+    status: "pending",
+  },
+  {
+    id: "4",
+    workflow_id: "1",
+    name: "task-4",
+    status: "running",
+  },
+  {
+    id: "5",
+    workflow_id: "2",
+    name: "task-5",
+    status: "pending",
+  },
+  {
+    id: "20",
+    parent_task: "3",
+    workflow_id: "1",
+    name: "task-20",
+    status: "running",
+  },
+  {
+    id: "30",
+    workflow_id: "1",
+    name: "task-30",
+    status: "running",
+  },
+  {
+    id: "21",
+    parent_task: "3",
+    workflow_id: "1",
+    name: "task-21",
+    status: "pending",
+  },
+  {
+    id: "6",
+    workflow_id: "2",
+    name: "task-6",
+    status: "pending",
+  },
+  {
+    id: "7",
+    workflow_id: "2",
+    name: "task-7",
+    status: "completed",
+  },
+  {
+    id: "8",
+    workflow_id: "3",
+    name: "task-8",
+    status: "completed",
+  },
+  {
+    id: "9",
+    workflow_id: "3",
+    name: "task-9",
+    status: "completed",
+  },
+  {
+    id: "10",
+    workflow_id: "4",
+    name: "task-10",
+    status: "running",
+  },
+  {
+    id: "11",
+    workflow_id: "4",
+    name: "task-11",
+    status: "running",
+  },
+  {
+    id: "12",
+    workflow_id: "4",
+    name: "task-12",
+    status: "running",
+  },
+  {
+    id: "13",
+    workflow_id: "5",
+    name: "task-13",
+    status: "completed",
+  },
+];
--- graphql-service/src/index.ts
+++ graphql-service/src/index.ts
@@ -1,11 +1,11 @@
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone"
+import { startStandaloneServer } from "@apollo/server/standalone";
 import { tasks, namespaces, workflows } from "./fake_data.js";
 
 const typeDefs = `#graphql
 
     type Workflow {
-        id: Int
+        id: String
         name: String
         tasks: [Task]
         status: String
@@ -28,7 +28,7 @@ const typeDefs = `#graphql
     }
 
     type Task {
-        id: Int
+        id: String
         workflow_id: Int
         name: String
         status: String
@@ -40,75 +40,88 @@ const typeDefs = `#graphql
         workflows(limit: Int, after: String, completed: Boolean, running: Boolean, pending: Boolean, failed: Boolean, namespace: String!): WorkflowConnection
     }
 
-`
+`;
 const resolvers = {
-    Query: {
-        workflows(_parent, args) {
-            let visitWorkflows = workflows.filter((workflow) => workflow.namespace === args.namespace);
-            if (args.completed !== undefined || args.running !== undefined || args.pending !== undefined || args.failed !== undefined) {
-                visitWorkflows = visitWorkflows.filter((workflow) => {
-                    if (args.completed && workflow.status === 'completed') {
-                        return true;
-                    }
-                    if (args.running && workflow.status === 'running') {
-                        return true;
-                    }
-                    if (args.pending && workflow.status === 'pending') {
-                        return true;
-                    }
-                    if (args.failed && workflow.status === 'failed') {
-                        return true;
-                    }
-                    return false;
-                });
-            }
+  Query: {
+    workflows(_parent, args) {
+      let visitWorkflows = workflows.filter(
+        (workflow) => workflow.namespace === args.namespace
+      );
+      if (
+        args.completed !== undefined ||
+        args.running !== undefined ||
+        args.pending !== undefined ||
+        args.failed !== undefined
+      ) {
+        visitWorkflows = visitWorkflows.filter((workflow) => {
+          if (args.completed && workflow.status === "completed") {
+            return true;
+          }
+          if (args.running && workflow.status === "running") {
+            return true;
+          }
+          if (args.pending && workflow.status === "pending") {
+            return true;
+          }
+          if (args.failed && workflow.status === "failed") {
+            return true;
+          }
+          return false;
+        });
+      }
 
-            let startIndex = 0;
-            if (args.after) {
-                const continueIndex = visitWorkflows.findIndex(workflow => workflow.id.toString() === args.after);
-                if (continueIndex >= 0) {
-                    startIndex = continueIndex + 1;
-                }
-            }
+      let startIndex = 0;
+      if (args.after) {
+        const continueIndex = visitWorkflows.findIndex(
+          (workflow) => workflow.id.toString() === args.after
+        );
+        if (continueIndex >= 0) {
+          startIndex = continueIndex + 1;
+        }
+      }
 
-            const limit = args.limit === undefined ? 10 : args.limit;
-            const paginatedWorkflows = visitWorkflows.slice(startIndex, startIndex + limit);
+      const limit = args.limit === undefined ? 10 : args.limit;
+      const paginatedWorkflows = visitWorkflows.slice(
+        startIndex,
+        startIndex + limit
+      );
 
-            const edges = paginatedWorkflows.map(workflow => ({
-                cursor: workflow.id.toString(),
-                node: workflow
-            }));
+      const edges = paginatedWorkflows.map((workflow) => ({
+        cursor: workflow.id.toString(),
+        node: workflow,
+      }));
 
-            const endCursor = edges.length > 0 ? edges[edges.length - 1].cursor : null;
-            const hasNextPage = visitWorkflows.length > startIndex + limit;
+      const endCursor =
+        edges.length > 0 ? edges[edges.length - 1].cursor : null;
+      const hasNextPage = visitWorkflows.length > startIndex + limit;
 
-            return {
-                edges,
-                pageInfo: {
-                    endCursor,
-                    hasNextPage,
-                    continue: hasNextPage ? endCursor : null
-                }
-            }
+      return {
+        edges,
+        pageInfo: {
+          endCursor,
+          hasNextPage,
+          continue: hasNextPage ? endCursor : null,
         },
-        namespaces(){
-            return namespaces
-        }
+      };
     },
-    Workflow: {
-        tasks(parent) {
-            return tasks.filter((task) => task.workflow_id === parent.id);
-        }
-    }
+    namespaces() {
+      return namespaces;
+    },
+  },
+  Workflow: {
+    tasks(parent) {
+      return tasks.filter((task) => task.workflow_id === parent.id);
+    },
+  },
 };
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+  typeDefs,
+  resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 }
+  listen: { port: 4000 },
 });
 
-console.log(`server ready at: ${url}`)
+console.log(`server ready at: ${url}`);
